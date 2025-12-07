8113db99440168965eac58843a1c3dca78085de9
Carlos <cbumang@student.mitt.ca>
2025-12-06 23:12:26 -0600

    Fixed elements for map, and added different font for h1 element on header

--- api-assignment.04/assets/script/script.js
+++ api-assignment.04/assets/script/script.js
@@ -1,4 +1,4 @@
 'use strict';
 
-mapboxgl.accessToken = "pk.eyJ1IjoiY2J1bWFuZ2xhZyIsImEiOiJjbWl1eGY4eG8xdWNrM2Rwd3R0OHdudTJwIn0.OXrnS1qNJok5oliucHDVBQ"
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2J1bWFuZ2xhZyIsImEiOiJjbWl1eGY4eG8xdWNrM2Rwd3R0OHdudTJwIn0.OXrnS1qNJok5oliucHDVBQ';
 
