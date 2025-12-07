31fbac7cd5c30d8c0e2aa512ce99a7bfbb97def6
Carlos <cbumang@student.mitt.ca>
2025-12-07 00:23:34 -0600

    Complete map, and fixed some styles to make it a desired size. I also added an icon image

--- api-assignment.04/assets/script/script.js
+++ api-assignment.04/assets/script/script.js
@@ -16,12 +16,38 @@ function findUser() {
 
   map.addControl(new mapboxgl.NavigationControl());
   mapElement.style.display = 'block';
+  map.resize();
 
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
       enableHighAccuracy: true
     })
   } else {
-    console.log('Geolocation is not supported by this browser.');
+    alert('Geolocation is not supported by this browser.');
   }
 };
+
+function successLocation(position) {
+  const longitude = position.coords.longitude;
+  const latitude = position.coords.latitude;
+  const userLocation = [longitude, latitude];
+
+  map.flyTo({
+    center: userLocation,
+    zoom: 15,
+    essential: true 
+  })
+
+  new mapboxgl.Marker({
+    color: "#3a24fa"
+})
+  .setLngLat(userLocation)
+  .addTo(map);
+}
+
+function errorLocation(error) {
+  console.log('Geolocation Error:', error.message);
+  alert('Could not get your exact location. Check your browser settings.');
+};
+
+btn.addEventListener('click', findUser);
