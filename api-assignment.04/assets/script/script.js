c90ba8fc63bfd06e385bf781502575ad7782afda
Carlos <cbumang@student.mitt.ca>
2025-12-06 23:36:21 -0600

    Creadted findUser() function and added variables

--- api-assignment.04/assets/script/script.js
+++ api-assignment.04/assets/script/script.js
@@ -2,3 +2,26 @@
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2J1bWFuZ2xhZyIsImEiOiJjbWl1eGY4eG8xdWNrM2Rwd3R0OHdudTJwIn0.OXrnS1qNJok5oliucHDVBQ';
 
+const btn = document.getElementById('toggleBtn');
+const mapElement = document.getElementById('location');
+let map;
+
+function findUser() {
+  map = new mapboxgl.Map({
+    container: 'location',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-97.1384, 49.8951],
+    zoom: 11
+  });
+
+  map.addControl(new mapboxgl.NavigationControl());
+  mapElement.style.display = 'block';
+
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+      enableHighAccuracy: true
+    })
+  } else {
+    console.log('Geolocation is not supported by this browser.');
+  }
+};
